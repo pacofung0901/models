665978a051cec9c3855c597938ef3a5427793436
pacokc901 <38418082+pacofung0901@users.noreply.github.com>
2023-06-27 12:11:57 +0800

    library fix

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -5,24 +5,24 @@ from setuptools import setup
 
 REQUIRED_PACKAGES = [
     # Required for apache-beam with PY3
-    'avro-python3',
-    'apache-beam',
-    'pillow',
-    'lxml',
-    'matplotlib',
-    'Cython',
-    'contextlib2',
-    'tf-slim',
-    'six',
-    'pycocotools',
-    'lvis',
-    'scipy',
-    'pandas',
-    'tf-models-official>=2.5.1',
-    'tensorflow_io',
-    'keras',
+    'avro-python3==1.10.2',
+    'apache-beam==2.43.0',
+    'pillow==7.0.0',
+    'lxml==4.5.0',
+    'matplotlib==3.6.2',
+    'Cython==0.29.33',
+    'contextlib2==21.6.0',
+    'tf-slim==1.1.0',
+    'six==1.14.0',
+    'pycocotools==2.0.6',
+    'lvis==0.5.3',
+    'scipy==1.10.0',
+    'pandas==1.5.2',
+    'tf-models-official==2.11.2',
+    'tensorflow_io==0.29.0',
+    'keras==2.11.0',
     'pyparsing==2.4.7',  # TODO(b/204103388)
-    'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
+    'sacrebleu==2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
 ]
 
 setup(
